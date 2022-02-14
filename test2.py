1eaab87f55816dc10e6f6ee8d3040e478f4d12bc
pmakeri.Peter Makeri <you@example.com>
2022-02-14 10:36:03 +0100

    test2 added

---
+++ test2.py
@@ -0,0 +1,13 @@
+
+def swap(s):
+    return s
+    s = input("Enter String to modify: \n")
+    print(f"String Before Modify {s}")
+    print("String After Modify:\n".swapcase(s))
+    
+print(swap("String Before Modify:\n Www.HackerRank.com"))
+print("Sring after Modify:\n")
+print("Www.HackerRank.com".swapcase())
+    
+    
+
